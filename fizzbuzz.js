ec490b50ce51a2c8704984d9e272d5d7bee1a21a
bthr-J <jkargo3@gmail.com>
2018-06-29 16:38:24 +0900

    modify fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,24 @@
 console.log('hello');
+
+
+
+
+
+
+
+function fizzbuzz(arr) {
+  var inputArr =[];
+  inputArr.push(arr);
+  inputArr.map(Element =>{
+    if(Element % 15 == 0){
+      console.log('FizzBuzz');
+    }else if(Element % 3 == 0){
+      console.log('Fizz');
+    }else if(Element % 5 == 0){
+      console.log('Buzz');
+    }else console.log(Element);
+  });
+}
+
+fizzbuzz(1);
+
